8f44243d1b3c6ba1440463579908f7b9445f3a1d
Martin Gerritsen <martin.gerritsen@gmail.com>
2025-12-18 22:51:57 +0100

    Chinese and Russian

--- apps/pecky.me/i18n/routing.ts
+++ apps/pecky.me/i18n/routing.ts
@@ -3,8 +3,7 @@ import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  // Future: 'zh' (Chinese), 'ru' (Russian)
-  locales: ['en', 'es'],
+  locales: ['en', 'es', 'zh', 'ru'],
 
   // Used when no locale matches
   defaultLocale: 'en',
